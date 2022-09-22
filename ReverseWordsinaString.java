c724497870aa06f7ca96e2df30300f403f23f410
Jegan0018 <112937116+Jegan0018@users.noreply.github.com>
2022-09-22 18:06:24 +0530

    Create ReverseWordsinaString.java

---
+++ ReverseWordsinaString.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reverseWords(String s) {
+        s=s.replaceAll("\\s+"," ").trim();
+        String[] str = s.split(" ");
+        String sm = "";
+		for(int i=str.length-1;i>=0;i--) {
+            if(i==0) {
+                sm += str[i];
+            }
+            else {
+                sm += str[i].trim()+" ";
+            }
+        }
+        return sm;
+    }
+}
