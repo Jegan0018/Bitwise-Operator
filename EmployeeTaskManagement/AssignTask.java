dc210c9badbbc8ef6de46f7c324cd5e2c7b65ce9
Jegan M <112937116+Jegan0018@users.noreply.github.com>
2022-10-05 12:44:23 +0530

    Update AssignTask.java

--- EmployeeTaskManagement/AssignTask.java
+++ EmployeeTaskManagement/AssignTask.java
@@ -4,46 +4,46 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
 import Database.DatabaseConnection;
 
 public class AssignTask {
 	public void assignTask() {
 		while (true) {
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("Enter Employee Id");
 			int id = 0;
 			try {
+				System.out.println("Enter Employee Id");
 				id = scanner.nextInt();
 			} catch (Exception e) {
-				System.out.println("***Please Enter Valid Number***");
+				System.err.println("***Please Enter Valid Number***");
 				break;
 			}
 			scanner.nextLine();
-			System.out.println("Enter Task Name");
+			System.out.println("Enter Task Name:");
 			String taskName = scanner.nextLine();
 			Pattern taskPattern = Pattern.compile("[a-zA-Z\\s,]+");
 			if (!taskPattern.matcher(taskName).matches()) {
-				System.out.println("***Invalid String***");
+				System.err.println("***Invalid String***");
 				break;
 			}
-			System.out.println("Enter Employee Task Issue Date");
+			System.out.println("Enter Employee Task Issue Date(Format:dd-mm-yyyy):");
 			String issueTaskDate = scanner.nextLine();
 			Pattern issueTaskDatePattern = Pattern.compile("^\\d{2}-\\d{2}-\\d{4}$");
 			if (!issueTaskDatePattern.matcher(issueTaskDate).matches()) {
-				System.out.println("Invalid Date Format");
+				System.err.println("***Invalid Date Format***");
 				break;
 			}
-			System.out.println("Enter Employee Task Due Date");
+			System.out.println("Enter Employee Task Due Date(Format:dd-mm-yyyy):");
 			String dueTaskDate = scanner.nextLine();
 			Pattern dueTaskDatePattern = Pattern.compile("^\\d{2}-\\d{2}-\\d{4}$");
 			if (!dueTaskDatePattern.matcher(dueTaskDate).matches()) {
-				System.out.println("***Invalid Date Format***");
+				System.err.println("***Invalid Date Format***");
 				break;
 			}
 			try {
 				Connection connection = DatabaseConnection.getConnection();
-				String sql = "Update emptasktable set taskName=?,issueTaskDate=?,dueTaskDate=?,status=0 where empId='" + id + "' ";
+				String sql = "Update emptasktable set taskName=?,issueTaskDate=?,dueTaskDate=?,status=0 where empId='"
+						+ id + "' ";
 				PreparedStatement statement = connection.prepareStatement(sql);
 				statement.setString(1, taskName);
 				statement.setString(2, issueTaskDate);
