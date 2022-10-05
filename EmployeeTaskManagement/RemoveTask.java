92aa768e8044eb9bd827d59f5d89b3cc650fe702
Jegan M <112937116+Jegan0018@users.noreply.github.com>
2022-10-05 12:45:47 +0530

    Update RemoveTask.java

--- EmployeeTaskManagement/RemoveTask.java
+++ EmployeeTaskManagement/RemoveTask.java
@@ -9,14 +9,14 @@ import Database.DatabaseConnection;
 
 public class RemoveTask {
 	public void removeTask() {
-		while(true) {
+		while (true) {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Enter Employee Id");
 			int id = 0;
 			try {
 				id = scanner.nextInt();
 			} catch (Exception e) {
-				System.out.println("***Please Enter Valid Number***");
+				System.err.println("***Please Enter Valid Number***");
 				break;
 			}
 			scanner.nextLine();
@@ -24,7 +24,7 @@ public class RemoveTask {
 			String taskName = scanner.nextLine();
 			Pattern taskPattern = Pattern.compile("[a-zA-Z\\s,]+");
 			if (!taskPattern.matcher(taskName).matches()) {
-				System.out.println("***Invalid String***");
+				System.err.println("***Invalid String***");
 				break;
 			}
 			try {
