842567e144e8994918e063242e7bb1681c52c21b
Jegan M <112937116+Jegan0018@users.noreply.github.com>
2022-10-15 09:41:19 +0530

    Update ArrayWeight.java

--- Assignments/ArrayWeight.java
+++ Assignments/ArrayWeight.java
@@ -13,24 +13,38 @@ public class ArrayWeight {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter Array Length");
 		int length = scanner.nextInt();
-		int[] array = new int[length];
+		int[] temp = new int[length];
+		System.out.println("Enter Array");
 		for (int i = 0; i < length; i++) {
-			array[i] = scanner.nextInt();
+			temp[i] = scanner.nextInt();
 		}
+		System.out.println("Enter Limit");
 		int limit = scanner.nextInt();
 		int count = 0, sum = 0;
-		Arrays.sort(array);
-		for (int i = 0; i < length; i++) {
-			sum = sum + array[i];
-			if (sum <= limit) {
-				continue;
-			} else {
-				count++;
-				sum = array[i];
+		Arrays.sort(temp);
+		int[] array=new int[temp.length];
+		int m=0;
+		for(int j=temp.length-1;j>=0;j--) {
+			array[m++]=temp[j];
+		}
+		int i=0,j=array.length-1;
+		int visited = 0;
+		while(i<j) {
+			if(array[i]+array[j]<=limit) {
+				array[j] += array[i];
+				array[i] = visited;
+				i++;
+				j--;
+			}
+			else {
+				i++;
 			}
 		}
-		if (sum != 0)
+	for(int k=0;k<array.length;k++) {
+		if(array[k]!=visited) {
 			count++;
-		System.out.println(count);
+		}
 	}
+	System.out.println(count);
+}
 }
