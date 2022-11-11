2dbbd92114e2ac8b65978dc0a623983eb887972f
Jegan M <112937116+Jegan0018@users.noreply.github.com>
2022-11-11 07:11:11 +0530

    Update ListContacts.java

--- ContactManagementApplication/ListContacts.java
+++ ContactManagementApplication/ListContacts.java
@@ -1,26 +1,33 @@
 package ContactApplication;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import Database.DatabaseConnection;
 
 public class ListContacts {
-	public void listContacts() {
-		list();
-	}
-
-	private void list() {
+	public void listContacts(ArrayList<ContactFields> arrayList) {
 		try {
 			DatabaseConnection.getInstance().connection = DatabaseConnection.getConnection();
-			String sql = "SELECT id,name,contactNumber,mailId FROM contacttable Order By name Asc";
+			String sql = "SELECT id,name,contactNumber,mailId FROM contactstable Order By name Asc";
 			PreparedStatement statement = DatabaseConnection.getInstance().connection.prepareStatement(sql);
 			ResultSet resultset = statement.executeQuery();
-			while (resultset.next()) {
-				System.out.println("Contact Name :" + resultset.getString("name"));
-				System.out.println("Contact Id :" + resultset.getString("id"));
-				System.out.println("Contact Number:" + resultset.getString("contactNumber"));
-				System.out.println("Mail Id :" + resultset.getString("mailId"));
+			if (arrayList.size() == 0) {
+				while (resultset.next()) {
+					arrayList.add(new ContactFields(resultset.getString("contactNumber"),resultset.getString("name"),resultset.getString("mailId")));
+				}
+			} else {
+				arrayList.removeAll(arrayList);
+				while (resultset.next()) {
+					arrayList.add(new ContactFields(resultset.getString("contactNumber"),resultset.getString("name"),resultset.getString("mailId")));
+				}
+			}
+			for(int index=0;index<arrayList.size();index++) {
+				ContactFields object=arrayList.get(index);
+				System.out.println("Contact Name :" + object.getName());
+				System.out.println("Contact Number:" + object.getContactNumber());
+				System.out.println("Mail Id :" + object.getMailId());
 				System.out.println("-------------------------------------");
 			}
 		} catch (Exception e) {
