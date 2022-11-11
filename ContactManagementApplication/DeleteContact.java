7a1c8bd4dc6234bbab1226218df0e39686d427dc
Jegan M <112937116+Jegan0018@users.noreply.github.com>
2022-11-11 07:09:23 +0530

    Update DeleteContact.java

--- ContactManagementApplication/DeleteContact.java
+++ ContactManagementApplication/DeleteContact.java
@@ -2,6 +2,8 @@ package ContactApplication;
 
 import java.sql.PreparedStatement;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 import Database.DatabaseConnection;
 
 public class DeleteContact {
@@ -11,22 +13,33 @@ public class DeleteContact {
 
 	private void delete() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please Enter the Id you want to Delete");
-		int id = 0;
-		try {
-			id = scanner.nextInt();
-		} catch (Exception e) {
-			System.err.println("***Please Enter Valid Number***");
-			delete();
-		}
+		String name;
+		boolean flag;
+		do {
+			System.out.println("Enter Existing Name");
+			name = scanner.nextLine();
+			flag = nameValidation(name);
+			if(flag == false) {
+				System.err.println("***Invalid String***");
+			}
+		} while(flag != true);
 
 		try {
 			DatabaseConnection.getInstance().connection = DatabaseConnection.getConnection();
-			String sql = "delete from contacttable where id = '"+id+"'";
+			String sql = "delete from contactstable where name = '"+name+"'";
 			PreparedStatement statement = DatabaseConnection.getInstance().connection.prepareStatement(sql);
 			statement.executeUpdate();
+			System.out.println("Deleted Successfully");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+
+	private boolean nameValidation(String name) {
+		Pattern namePattern = Pattern.compile("[a-zA-Z\\s,]+");
+		if(!namePattern.matcher(name).matches()) {
+			return false;
+		}
+		return true;
 	}
 }
